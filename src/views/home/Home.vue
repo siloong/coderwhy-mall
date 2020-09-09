fa132cc8a65e8bec39f1c166af54f3737b5a312d
siloong <2296926656@qq.com>
2020-09-09 14:56:10 +0800

    updata swiper component

---
+++ src/views/home/Home.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="home">
+    <nav-bar class="home-nav"><div>购物街</div></nav-bar>
+    <home-swiper :banners="banners"></home-swiper>
+    <recommend-view :recommends="recommends"></recommend-view>
+  </div>
+</template>
+
+
+<script>
+import {getHomeMultidata} from 'network/home'
+
+import NavBar from 'components/common/navbar/NavBar'
+import HomeSwiper from './childComps/HomeSwiper'
+import RecommendView from './childComps/RecommendView'
+
+export default {
+  name: 'Home',
+  data() {
+    return {
+      banners: null,
+      recommends: null
+    }
+  },
+  components: {
+    NavBar,
+    HomeSwiper,
+    RecommendView
+  },
+  created() {
+    getHomeMultidata().then(res => {
+      // console.log(res);
+      this.banners = res.data.banner.list;
+      this.recommends =res.data.recommend.list;
+    });
+  }
+}
+</script>
+
+
+<style scoped>
+  .home-nav {
+    color: #fff;
+    background-color: var(--color-tint);
+  }
+</style>
---
+++ src/views/home/childComps/RecommendView.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="recommend">
+    <div class="recommend-item"
+    v-for="(item, index) in recommends"
+    :key="index">
+      <a :href="item.link">
+        <div>
+          <img :src="item.image" alt="">
+        </div>
+        <div class="item-title">{{item.title}}</div>
+      </a>
+    </div>
+  </div>
+</template>
+
+
+<script>
+  export default {
+    name: "RecommendView",
+    props: {
+      recommends: {
+        type: Array,
+        default() {
+          return [];
+        }
+      }
+    }
+  }
+</script>
+
+
+<style>
+  .recommend {
+    display: flex;
+  }
+
+  .recommend-item {
+    flex: 1;
+    text-align: center;
+    padding: 6px;
+    box-sizing: border-box;
+  }
+
+  .recommend-item img {
+    width: 100%;
+    padding-bottom: 2px;
+  }
+
+  .recommend-item .item-title {
+    font-size: 14px;
+  }
+</style>
